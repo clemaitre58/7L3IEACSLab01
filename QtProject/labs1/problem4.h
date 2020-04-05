2c61a12dae776d5a086bc5650df9e042b5bddbf8
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-05 19:44:40 +0800

    Update problem4.h

--- QtProject/labs1/problem4.h
+++ QtProject/labs1/problem4.h
@@ -1,4 +1,16 @@
 #ifndef PROBLEM4_H
 #define PROBLEM4_H
 
+////Bubble Sort------------
+//
+void PrintArray(int *array, int n);
+
+void BubbleSort(int *array, int n);
+
+
+//Quick Sort--------------
+
+
+
+
 #endif // PROBLEM4_H
