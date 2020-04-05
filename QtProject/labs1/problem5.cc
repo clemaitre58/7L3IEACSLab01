b9b1de1204bf3ee6291ef4d6533da6b6c9eef229
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-05 19:49:50 +0800

    Update problem5.cc

--- QtProject/labs1/problem5.cc
+++ QtProject/labs1/problem5.cc
@@ -1,1 +1,16 @@
 #include "problem5.h"
+
+void calculate_transpose(int matrix[6][6], int rows, int cols);
+
+ long fact(int x)
+{
+    int i;
+    long f=1;
+
+    for(i=1;i<=x;++i)
+    {
+        f=f*i;
+    }
+
+    return f;
+}
