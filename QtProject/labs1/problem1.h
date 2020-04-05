332a8ca8c639e3b22b56d938f9ab5e5b6c17ecc6
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-05 13:07:34 +0800

    Update problem1.h

--- QtProject/labs1/problem1.h
+++ QtProject/labs1/problem1.h
@@ -1,7 +1,25 @@
 #ifndef PROBLEM1_H
 #define PROBLEM1_H
 
+//
+// Swapping variables
+int swapp(int num1, int num2);
+//
+// checking for sign change
+int sign_change(int num1, int num2);
+//
+//checking even or odd
+int finding_odd_even(int num);
+//
+//
+// printing "first" , "second"  by choice
+int first();
+int second();
 
-// prototype for my function
-void example_function();
+
+
+
+
+
 #endif // PROBLEM1_H
+
