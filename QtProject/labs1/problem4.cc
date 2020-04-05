e62d16b234ef2cf64cd31cc281ba3df79f8184cd
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-05 22:29:53 +0800

    Update problem4.cc

--- QtProject/labs1/problem4.cc
+++ QtProject/labs1/problem4.cc
@@ -32,7 +32,39 @@ void BubbleSort(int *array, int n) {
 
 
  //QUICK SORT------------
+void printArray(int *array, int n)
+{
+    for (int i = 0; i < n; ++i)
+        std::cout << array[i] << std::endl;
+}
 
+void quickSort(int *array, int low, int high)
+{
+    int i = low;
+    int j = high;
+    int pivot = array[(i + j) / 2];
+    int temp;
+
+    while (i <= j)
+    {
+        while (array[i] < pivot)
+            i++;
+        while (array[j] > pivot)
+            j--;
+        if (i <= j)
+        {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    if (j > low)
+        quickSort(array, low, j);
+    if (i < high)
+        quickSort(array, i, high);
+}
 
 
 
