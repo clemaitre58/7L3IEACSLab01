bd518f383cae97393ffb3ab66906506e1103cf2d
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-05 19:46:35 +0800

    Update problem4.cc

--- QtProject/labs1/problem4.cc
+++ QtProject/labs1/problem4.cc
@@ -1,16 +1,38 @@
-#ifndef PROBLEM4_H
-#define PROBLEM4_H
+#include "problem4.h"
+#include <iostream>;
 
-////Bubble Sort------------
+
+
+////Bubble Sort---------
 //
-void PrintArray(int *array, int n);
+void PrintArray(int *array, int n) {
+  for (int i = 0; i < n; ++i)
+    std::cout << array[i] << " " << std::flush;
+  std::cout << std::endl;
+}
+//
+void BubbleSort(int *array, int n) {
+  bool swapped = true;
+  int j = 0;
+  int temp;
 
-void BubbleSort(int *array, int n);
+  while (swapped) {
+    swapped = false;
+    j++;
+    for (int i = 0; i < n - j; ++i) {
+      if (array[i] > array[i + 1]) {
+        temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  }
+}
 
 
-//Quick Sort--------------
+ //QUICK SORT------------
 
 
 
 
-#endif // PROBLEM4_H
