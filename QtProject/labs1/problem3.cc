31f0d5b5d8ff1030f8f210be90af01e3b3a27c83
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-05 18:32:05 +0800

    Update problem3.cc

--- QtProject/labs1/problem3.cc
+++ QtProject/labs1/problem3.cc
@@ -1,1 +1,21 @@
 #include "problem3.h"
+#include <iostream>;
+
+int* swapp(int num1, int num2){
+int t =num1;
+num1=num2;
+num2=t;
+std::cout<<"integer_1="<<num1<<",integer_2="<<num2<<std::endl;
+
+return 0;
+}
+
+
+int& swapp(int num1, int num2){
+int t =num1;
+num1=num2;
+num2=t;
+std::cout<<"integer_1="<<num1<<",integer_2="<<num2<<std::endl;
+
+
+}
